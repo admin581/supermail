c23e35097ed144dee752dd576045651f7d63769d
admin581 <2683377057@qq.com>
2023-09-15 15:19:11 +0800

    初始换文件

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+module.exports = {
+  configureWebpack : {
+    resolve : {
+      alias : {
+        // @表示src
+        'assets' : '@/assets',
+        'common' : '@/common',
+        'components' : '@/components',
+        'network' : '@/network',
+        'views' : '@/views'
+      }
+    }
+  }
+}
