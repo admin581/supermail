12ba745c4fe6dd7d2a09c95ecdf08158a75af16f
admin581 <2683377057@qq.com>
2023-09-15 16:03:26 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <main-tab-bar></main-tab-bar>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
